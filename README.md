d313b9487b9af5bcdeec30f16454c573aeac105d
goncy <gonzalo.pozzo4@gmail.com>
2022-09-10 15:10:46 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Figuncy
+
+Te avisa si hay figuritas con una notificación del fs.
+
+* Instalá Node.js
+* Instalá las dependencias con `npm install`
+* Corre `npm start`
+
+Corre cada un minuto.
---
+++ index.ts
@@ -0,0 +1,24 @@
+import { chromium } from 'playwright'
+import notifier from 'node-notifier'
+import cron from 'node-cron'
+
+cron.schedule("* * * * *", async () => {
+  console.log(`Running on: ${new Date().toLocaleString('es-AR', { timeZone: 'America/Buenos_Aires' })}`)
+
+  const browser = await chromium.launch()
+  const page = await browser.newPage()
+  await page.goto('https://www.zonakids.com/productos/pack-x-25-sobres-de-figuritas-fifa-world-cup-qatar-2022/')
+
+  const content = await page.inputValue('#product_form input[type="submit"]')
+
+  if (content === 'Sin stock') {
+    console.log('SIN STOCK')
+  } else {
+    notifier.notify({
+      title: 'HAY FIGURITAS!!',
+      message: `Se detectó stock en el pack x25 sobres de Panini`
+    })
+  }
+  
+  await browser.close()
+})
