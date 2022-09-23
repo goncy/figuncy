7a57571ad26d10c2c95b061999d248b936aae4eb
goncy <gonzalo.pozzo4@gmail.com>
2022-09-23 17:25:54 -0300

    Udpate bot

--- index.ts
+++ index.ts
@@ -2,23 +2,29 @@ import { chromium } from 'playwright'
 import notifier from 'node-notifier'
 import cron from 'node-cron'
 
-const browser = await chromium.launch()
-cron.schedule("* * * * *", async () => {
-  console.log(`Running on: ${new Date().toLocaleString('es-AR', { timeZone: 'America/Buenos_Aires' })}`)
+async function main() {
+  const browser = await chromium.launch()
 
-  const page = await browser.newPage()
-  await page.goto('https://www.zonakids.com/productos/pack-x-25-sobres-de-figuritas-fifa-world-cup-qatar-2022/')
+  cron.schedule("* * * * *", async () => {
+    console.log(`Running on: ${new Date().toLocaleString('es-AR', { timeZone: 'America/Buenos_Aires' })}`)
 
-  const content = await page.inputValue('#product_form input[type="submit"]')
+    const page = await browser.newPage()
+    await page.goto('https://www.zonakids.com/productos/pack-x-25-sobres-de-figuritas-fifa-world-cup-qatar-2022/')
+    
+    const meta = page.locator('meta[property="tiendanube:stock"]');
+    const content = await meta.getAttribute("content")
 
-  if (content === 'Sin stock') {
-    console.log('SIN STOCK')
-  } else {
-    notifier.notify({
-      title: 'HAY FIGURITAS!!',
-      message: `Se detectó stock en el pack x25 sobres de Panini`
-    })
-  }
-  
-  await page.close()
-})
+    if (content === '0') {
+      console.log('SIN STOCK')
+    } else {
+      notifier.notify({
+        title: 'HAY FIGURITAS!!',
+        message: `Se detectó stock en el pack x25 sobres de Panini`
+      })
+    }
+    
+    await page.close()
+  })
+}
+
+main()
